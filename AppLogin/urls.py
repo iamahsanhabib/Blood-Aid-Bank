304e4f277ad177a097790961f9ab3c2701eb07c8
Ahsan Habib <ahsan.just.cse@gmail.com>
2021-06-05 02:16:51 +0600

    12

--- AppLogin/urls.py
+++ AppLogin/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
+    path('profile/', views.user_profile, name='profile'),
+    path('change-profile/', views.change_user_profile, name='change_profile'),
+    path('password/', views.change_user_password, name='change_password'),
     
 ]
--- AppLogin/views.py
+++ AppLogin/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login, logout, authenticate
-from django.shortcuts import HttpResponseRedirect, HttpResponse
+from django.shortcuts import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from AppLogin.forms import UserProfileChange
 
 def user_login(request):
     form = AuthenticationForm()
@@ -22,3 +23,32 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect('/')
+
+@login_required
+def user_profile(request):
+    return render(request, 'AppLogin/profile.html', context={})
+
+@login_required
+def change_user_profile(request):
+    current_user = request.user
+    form = UserProfileChange(instance = current_user)
+    if request.method == 'POST':
+        form  = UserProfileChange(request.POST, instance = current_user)
+        if form.is_valid():
+            form.save()
+            form = UserProfileChange(instance = current_user)
+            return redirect('/account/profile/')
+    return render(request, 'AppLogin/change_profile.html', context={'form': form})
+
+@login_required
+def change_user_password(request):
+    current_user = request.user
+    form = PasswordChangeForm(current_user)
+    changed = False
+    if request.method == 'POST':
+        form = PasswordChangeForm(current_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            changed = True
+    return render(request, 'AppLogin/change_password.html', context={'form': form, 'changed': changed})        
+
